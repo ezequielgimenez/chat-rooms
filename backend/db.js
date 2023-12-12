a09cc7aebc118f6041be29a43c29117b2311033d
Ezequiel <ezequielezequiel9@gmail.com>
2023-12-11 23:18:12 -0300

    change env vars

--- backend/db.js
+++ backend/db.js
@@ -2,23 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.rtdb = exports.firestore = void 0;
 const admin = require("firebase-admin");
+// import * as serviceAccount from "./serviceAccount.json";
 const dotenv = require("dotenv");
 dotenv.config();
-const serviceAccount = {
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url,
-    universe_domain: process.env.universe_domain,
-};
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
     databaseURL: process.env.dataBaseRTDB,
 });
 const firestore = admin.firestore();
